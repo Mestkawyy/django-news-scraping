d9870b992ceaaa2094b4f373d2e0eb2267903f31
Ahmed Aly <65461439+Mestkawyy@users.noreply.github.com>
2022-08-29 09:12:02 -0700

    Add files via upload

---
+++ news_suggester/scrapper.py
@@ -0,0 +1,73 @@
+import requests
+
+
+def NewsApi():
+    global news_list
+    key = "f58bfbf749394f80bae8416e36329597"
+    url = "http://newsapi.org/v2/top-headlines?country=in&apiKey=" + key
+
+    res = requests.get(url)
+    if str(res.json()['status']).lower() == 'ok':
+        news_list = []
+        for i in range(1, len(res.json()['articles']) + 1):
+            try:
+                each_news = dict()
+                each_news['headlines'] = res.json()['articles'][i - 1]['title']
+                each_news['description'] = res.json()['articles'][i - 1]['description']
+                each_news['author'] = res.json()['articles'][i - 1]['author'] if res.json()['articles'][i - 1][
+                                                                                     'author'] != None else "Unknown"
+                each_news['source_name'] = res.json()['articles'][i - 1]['source']['name']
+                each_news['source_link'] = res.json()['articles'][i - 1]['url']
+                each_news['thumbnail'] = res.json()['articles'][i - 1]['urlToImage']
+
+                news_list.append(each_news)
+            except:
+                pass
+
+    return news_list
+
+
+class NewsScrapper:
+    def __init__(self):
+        pass
+
+    # It Scrapes news from newsapi.org website
+
+    def GNewsApi(self):
+        key = "Enter your API key here"
+        url = f"https://gnews.io/api/v4/search?q=india&token={key}&lang=en"
+
+        res = requests.get(url)
+        if True:
+            news_list = []
+            for i in range(1, len(res.json()['articles']) + 1):
+                try:
+                    each_news = dict()
+                    each_news['headlines'] = res.json()['articles'][i - 1]['title']
+                    each_news['description'] = res.json()['articles'][i - 1]['description']
+                    each_news['author'] = "Unknown"
+                    each_news['source_name'] = res.json()['articles'][i - 1]['source']['name']
+                    each_news['source_link'] = res.json()['articles'][i - 1]['url']
+                    each_news['thumbnail'] = res.json()['articles'][i - 1]['image']
+
+                    news_list.append(each_news)
+                except:
+                    pass
+
+        return news_list
+
+    def fetch_all_news(self):
+        all_news = []
+        try:
+            all_news += NewsApi()
+        except:
+            pass
+        try:
+            all_news += self.GNewsApi()
+        except:
+            pass
+
+        return all_news
+
+# a  = NewsScrapper()
+# print(a.fetch_all_news())
